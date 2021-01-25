68c1368fc521597eccf32353615d599f6aab80dd
JonathanDhoop <hungryguybs@gmail.com>
2021-01-25 11:45:23 +0100

    create character + walking

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact,
+    sneak
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk;
+        anim = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+        else
+        {
+            
+        }
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+        myRigidBody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
